cf6a1f41300c7cc1ccfeaf0febe21a7a2b4a558f
Ryan G Ferris <ryanglenferris@gmail.com>
2018-01-29 11:05:29 -0800

    Create SQL Commands.

---
+++ src/main/java/models/User.java
@@ -0,0 +1,50 @@
+package models;
+
+/**
+ * Created by Guest on 1/29/18.
+ */
+public class User {
+
+    //LOGIN
+    private int id;
+    private String userName;
+    private String userEmail;
+    private String password;
+
+    private String firstName;
+    private String lastName;
+    private String birthday;
+    private String gender;
+
+    private String astrologicalSign;
+    private String userTagLine;
+    private String userBio;
+
+    private img profilePicture;
+    private String job;
+    private String education;
+
+    private String perfectDate;
+    private String dateIdeas;
+
+    private String interests;
+    private String interestsV2; //do you want kids?
+    private String interestsV3; //furries?
+    private String
+
+/*
+PROFILE
+USER
+LOGIN
+INTERESTS
+
+ACTIVITIES
+
+
+ */
+
+    //    FOREIGN KEYS
+    private int loginId;
+
+
+}
--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,22 +1,112 @@
-
 SET MODE PostgreSQL;
 
-CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user VARCHAR,
---  FOREIGN KEYS
+CREATE TABLE IF NOT EXISTS login (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR,
+  email VARCHAR,
+  password VARCHAR,
+  birthday TIMESTAMP NULL DEFAULT NULL
 );
 
-CREATE TABLE IF NOT EXISTS genders (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    gender VARCHAR,
---  FOREIGN KEYS
-    userId INTEGER
+--USER INFO------------------------------------------------
+CREATE TABLE IF NOT EXISTS user (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  firstName VARCHAR,
+  lastName VARCHAR,
+  gender VARCHAR,
+  tagLine VARCHAR,
+  --foreign keys
+  loginId INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS sexes (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    sex VARCHAR,
---  FOREIGN KEYS
-    userId INTEGER
+--PROFILE INFO------------------------------------------------
+CREATE TABLE IF NOT EXISTS profile (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  profilePicture VARCHAR,
+  aboutMe VARCHAR,
+  --foreign keys
+  userId INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS job (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  job VARCHAR,
+  position VARCHAR,
+  description VARCHAR,
+);
+
+CREATE TABLE IF NOT EXISTS education (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  education VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS aboutMe (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  about VARCHAR
+);
+
+--GLOBAL TABLES------------------------------------------------
+CREATE TABLE IF NOT EXISTS interests (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  interest VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS singles (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+);
+
+CREATE TABLE IF NOT EXISTS dateIdeas (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  dateTitle VARCHAR,
+  description VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  category VARCHAR,
+);
+
+--JOIN TABLES:USER ------------------------------------------------
+CREATE TABLE IF NOT EXISTS users_singles (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  --foreign keys
+  userId  INTEGER,
+  singlesId  INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS user_interests (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+    --foreign keys
+  interestsId  INTEGER,
+  userId  INTEGER
+);
+
+--JOIN TABLES:PROFILE------------------------------------------------
+CREATE TABLE IF NOT EXISTS profile_job (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+    --foreign keys
+  profileId  INTEGER,
+  jobId  INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS profile_education (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+    --foreign keys
+  profileId  INTEGER,
+  educationId  INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS profile_aboutMe (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+    --foreign keys
+  profileId  INTEGER,
+  aboutMeId  INTEGER
+);
+
+--JOIN TABLES:INTERESTS ------------------------------------------------
+CREATE TABLE IF NOT EXISTS interests_categories (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+    --foreign keys
+  interestsId  INTEGER,
+  categoriesId  INTEGER
 );
