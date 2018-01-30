7e33682f4d11cf6223f0a4d1288803c324bd9ce2
kyle tenzeldam <ktenzel@gmail.com>
2018-01-30 14:52:41 -0800

    updates to css and login.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,81 @@
+import dao.LoginSQL;
+import models.Login;
+import models.User;
+import org.sql2o.Sql2o;
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import dao.*;
+import spark.ModelAndView;
 
-/**
- * Created by Guest on 1/29/18.
- */
+import static spark.Spark.*;
+import static spark.Spark.staticFileLocation;
+
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String connectionString = "jdbc:h2:~/dad.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        InterestSQL interestDao = new InterestSQL(sql2o);
+        UserSQL userDao = new UserSQL(sql2o);
+        LoginSQL loginDao = new LoginSQL(sql2o);
 
 
-    get("/", (request, response) -> {
-        Map<String, Object> model = new HashMap<>();
-        return new ModelAndView(model, "index.hbs");
-    }, new HandlebarsTemplateEngine());
+        //HOME PAGE/LOGIN
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+
+            return new ModelAndView(model, "login.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //SHOW EXISTING USER AFTER LOGIN
 
 
 
+        //NEW USER FORM/SIGN UP
+        get("/user/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "sign-up.hbs");
+        }, new HandlebarsTemplateEngine());
 
 
-    get("/login", (request, response) -> {
-        Map<String, Object> model = new HashMap<>();
-        return new ModelAndView(model, "index.hbs");
-    }, new HandlebarsTemplateEngine());
+
+        //PROCESS NEW USER
+//        post("/user/new", (request, response)->{
+//            Map<String, Object> model = new HashMap<>();
+//            String userName = request.queryParams("userName");
+//            String password = request.queryParams("password");
+//            String birthday = request.queryParams("birthday");
+//            Login newLogin = new Login(userName, password, birthday);
+//
+//            int loginId = newLogin.getId();
+//            String name = request.queryParams("name");
+//            String gender = request.queryParams("gender");
+//            String preference = request.queryParams("preference");
+//            String userTagLine = request.queryParams("userTagLine");
+//            User newUser = new User(loginId, name, gender, preference);
+//
+//            userDao.add(newUser);
+//            model.put("user", newUser);
+//            return new ModelAndView(model, "welcome.hbs");
+//        }, new HandlebarsTemplateEngine());
+
+        //UPDATE USER/EXPAND PROFILE
+
+        //SHOW ALL USERS BY PREFERENCE
+
+        //SHOW A SELECTED USER'S PROFILE
+
+        //LIKE A SELECTED USER
+
+        //
+    }
 }
